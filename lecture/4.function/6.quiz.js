1433db4bcdf2cd923839150eb2019882b91225fe
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-11-17 07:32:22 +0900

    Javascript 새벽공부 시작

--- lecture/4.function/6.quiz.js
+++ lecture/4.function/6.quiz.js
@@ -1,13 +1,6 @@
 'use strict';
 // 0부터 number까지 순회를 하면서 어떠한 동작을 하게 만드는 함수! 
 
-function multy(number){
-    return number *= 2;
-}
-function iterate(number){
-    for(let i = 0; i <=number; i++){
-        multy(i);
-    }
-}
-// 함수가 2개 있으면 그 첫 번째 함수안에 2번째 함수를 넣어준다 => 파라미터로 넣어준다.
-iterate(3, multy); // => 이런식의 callback함수를 호출 가능!! 
+let number = 3;
+
+// 자바스크립트 시작! 
