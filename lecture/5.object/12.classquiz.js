4577135647e2ea5b11dd7bdf7561d261055728bc
jisung24 <jisung4012@naver.com>
2022-11-21 00:43:07 +0900

    class 사용 이유 + 어떻게 사용을 해야하는지~ carousel클래스로 한 번 살펴봄

--- lecture/5.object/12.classquiz.js
+++ lecture/5.object/12.classquiz.js
@@ -1,12 +1,34 @@
 'use strict';
 
-// 카운터 만들기 
-// 0이상의 값으로 초기화 한 뒤 하나씩 숫자를 증가시켜주는 카운터를 만든다!! 
-// => static
+// 캐러셀 class만들기! 
+class Carousel{
+    constructor(currPages, allPages){
+        this.currPages = currPages;
+        this.allPages = allPages;
+    }
+    // get set으로 일단 예외처리를 해 준다. 
+    // get currPages(){
+    //     return this.currPages;
+    // }
+    // set currPages(val){
+    //     val <= 0 ? 'console.log(`페이지는 0보단 커야합니다.`)' : this.currPages = val;
+    //     // 입력받은 현재 페이지가 0보다 클때만, 입력을 받아주고, 아니면 걸러준다. 
+    // }
 
-// 만약 우리가 프로젝트를 할 때 숫자를 하나씩 증가시켜주는 class를 잘 만들어놓는다면
-// 아주아주 편할거같아......
+    // 메서드 
+    // 페이지 나타내주는 메서드.
+    curr = () => {
+        console.log(`현재 페이지는  ${this.currPages}입니다.`);
+    }
+    // 이동시켜주는 메서드
+    next = () => {
+        console.log(`지금 페이지 >> ${this.currPages + 1}번 됨.`);
+    }
+}
 
-// user data 
+let car1 = new Carousel(1, 5); // 현재 1페이지, 총 5페이지
+car1.next(); // 다음 페이지 이동! (2번 됨);
 
-// 자바스크립트 복습 시작(2022-11-20)
+// 그니까 어떤 기능을 하나 잡아서 그 물체의 기능과 변수들을 전부 class안에 때려박아!! --> 이런식으로 class우리 한 번 작성해보자!!
+
+// ex) 학생관련한 학생 class => 학생에 대한 기능도 있어야 돼! 
