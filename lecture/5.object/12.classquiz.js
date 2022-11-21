ec80ebc711181f8dbc1c736fbe81dfef56a7550f
jisung24 <jisung4012@naver.com>
2022-11-21 13:38:14 +0900

    javascript에서 class가 프로젝트에서 어떻게 실질적으로 사용되는지 생각해보기?

--- lecture/5.object/12.classquiz.js
+++ lecture/5.object/12.classquiz.js
@@ -1,34 +1,21 @@
-'use strict';
-
-// 캐러셀 class만들기! 
-class Carousel{
-    constructor(currPages, allPages){
-        this.currPages = currPages;
-        this.allPages = allPages;
+class Notebook{
+    // class는 javascript에서 함수이다.
+    constructor(title, company, price, size){
+        this.title = title;
+        this.company = company;
+        this.price = price;
+        this.size = size;
     }
-    // get set으로 일단 예외처리를 해 준다. 
-    // get currPages(){
-    //     return this.currPages;
-    // }
-    // set currPages(val){
-    //     val <= 0 ? 'console.log(`페이지는 0보단 커야합니다.`)' : this.currPages = val;
-    //     // 입력받은 현재 페이지가 0보다 클때만, 입력을 받아주고, 아니면 걸러준다. 
-    // }
-
-    // 메서드 
-    // 페이지 나타내주는 메서드.
-    curr = () => {
-        console.log(`현재 페이지는  ${this.currPages}입니다.`);
+    // getter setter(예외처리)
+    get title(){
+        return this.title + 'won';
     }
-    // 이동시켜주는 메서드
-    next = () => {
-        console.log(`지금 페이지 >> ${this.currPages + 1}번 됨.`);
+    set setPrice(price){
+        price < 100000 ? console.log('가격이 올바르지 않습니다.') : this.price = price; 
+    }
+    // method
+    intro = () => {
+        // 노트북 사양소개 시켜주는 메서드.
+        console.log(`title >> ${this.title}, company >> ${this.company}, price >> ${this.price}, size >> ${this.size}`);
     }
 }
-
-let car1 = new Carousel(1, 5); // 현재 1페이지, 총 5페이지
-car1.next(); // 다음 페이지 이동! (2번 됨);
-
-// 그니까 어떤 기능을 하나 잡아서 그 물체의 기능과 변수들을 전부 class안에 때려박아!! --> 이런식으로 class우리 한 번 작성해보자!!
-
-// ex) 학생관련한 학생 class => 학생에 대한 기능도 있어야 돼! 
