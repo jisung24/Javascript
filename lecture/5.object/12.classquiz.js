2d912b682c694ca431db47955d985852a4141bc4
jisung24 <jisung4012@naver.com>
2022-11-22 21:46:00 +0900

    2022-11-22 강의 시작

--- lecture/5.object/12.classquiz.js
+++ lecture/5.object/12.classquiz.js
@@ -1,21 +1,26 @@
-class Notebook{
-    // class는 javascript에서 함수이다.
-    constructor(title, company, price, size){
-        this.title = title;
-        this.company = company;
-        this.price = price;
-        this.size = size;
+class User{
+    #id = 0; // 외부에서 유져 id를 건들 수 없음. private으로! 
+    #email;
+    #password; // email과 password는 private으로 => 외부에서 건들 수 없게...!! 
+    constructor(name, age, email, password){ // 이름 나이 이메일 비밀번호
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        // javascript공부 시작!
     }
-    // getter setter(예외처리)
-    get title(){
-        return this.title + 'won';
+
+    // getter setter로 예외처리
+    get _age(){
+        return this.age; // 입력받은 나이 return해줌. 
     }
-    set setPrice(price){
-        price < 100000 ? console.log('가격이 올바르지 않습니다.') : this.price = price; 
+    set _age(age){
+        age > 19 ? this.age = age : console.log('미성년자는 가입할 수 없습니다.');
     }
+
     // method
     intro = () => {
-        // 노트북 사양소개 시켜주는 메서드.
-        console.log(`title >> ${this.title}, company >> ${this.company}, price >> ${this.price}, size >> ${this.size}`);
+        console.log(`제 이름은 >> ${this.name}이고 나이는 ${this.age}입니다.!!`);
     }
+    
 }
