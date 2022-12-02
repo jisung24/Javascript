6c217e836060347d8fb4f5c2932407237c58ba15
jisung24 <jisung4012@naver.com>
2022-12-03 01:13:12 +0900

    객체가 필요한 이유

--- lecture/8.iteration(반복,순회)/4.destructure.js
+++ lecture/8.iteration(반복,순회)/4.destructure.js
@@ -1,47 +1,17 @@
 'use strict';
-// destructure : 구조분해할당! 
+// destructure (배열, 객체만!)
 // ⭐️ spread 연산자와 많이 사용됨! 
-// - data 그룹화를 쉽게 할 수 있다. 
+// - 배열이나 객체의 속성을 해체하고, 그 개별변수에 담을 수 있게하는 표현식. 
 
-const arr = [2,4,6,8,10];
-const [first, second, ...others] = [2,3,6,8,10];
-console.log(others);
-// 음.. 배열 원소지만, 그 안에서도 좀 더 의미를 더하기 위해서, 사용함. 
-// 배열 안에서도 조금 더 의미있는 이름을 전달할 수 있어. 
-const [x, y, z = 0] = [1, 2, 8];
-// 할당을 해줘도 값이 있다면 그게 우선이라 8이 들어감. 
-console.log(x, y, z);
-
-
-// obj
-// 이렇게 변수를 객체안에 넣어서 key를 따로 분해해주는 이거 상당히 많이 사용함. 
-// 객체의 me.age가 아니라 바로 age로 사용이 가능하다.
-
-class Jisung{
-    constructor(name, age, school){
-        this.name = name;
-        this.age = age;
-        this.school = school;
+// 객체 구조분해할당 
+// : 객체의 key들을 싹 다 분리시켜준다. 
+let jisung = { 
+    name : "지성",
+    age : 25,
+    major : "computer engineering",
+    intro : () => {
+        return console.log(`${this.name}이의 나이 >> ${this.age}, 전공 >> ${this.major}입니다.`);
     }
 }
-
-const jisung = new Jisung("김지성", 25, "가천대학교");
-let { name, age, school } = jisung;
-console.log(name, age, school);
-
-
-// quiz 
-function changeColor(color){
-    console.log(color);
-}
-
-const prop = {
-    names : "button",
-    styles : {
-        size : 20,
-        color : "black",
-    }
-}
-const { names } = prop;
-const { size, color } = prop.styles;
-changeColor(color); // 바로 넘겨준다.
+console.log(jisung);
+console.log(jisung.intro());
